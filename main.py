180c851222b8b42cf22cec36b05c798334f16706
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-10 12:36:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import pandas as pd
 
 st.header('Test for Importing and Exporting Data to and from Google Sheets')
 data = pd.read_csv("https://docs.google.com/spreadsheets/d/1RPBl2AuY5UVqp_ay8huKWGYNlIfRM2Zb6KbK2I28xrs/gviz/tq?tqx=out:csv")
-df = pd.DataFrame(data)
 
+df = pd.DataFrame(data)
 st.dataframe(df) 
 
 teamNumber = st.number_input('Team Number')
 st.write(teamNumber)
 
-autoPoints = st.number_input('Points during Auto:')
-st.write(autoPoints)
+sampleData1 = st.number_input(key = 1, label = 'Sample Data 1:')
+st.write(sampleData1)
+
+sampleData2 = st.number_input(key = 2, label = 'Sample Data 2:')
+st.write(sampleData2)
+
+sampleData3 = st.number_input(key = 3, label = 'Sample Data 3:')
+st.write(sampleData3)
 
