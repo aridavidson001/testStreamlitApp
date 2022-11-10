a3321eae4cdbab5593ea0c17e14532bb86f5bcd9
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-10 15:40:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,5 +17,21 @@ def run_query(query):
 sheet_url = st.secrets["public_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-# Print results.
-st.write(rows)
+
+st.header('Test for Importing and Exporting Data to and from Google Sheets')
+data = pd.read_csv("sheet_url")
+
+df = pd.DataFrame(data)
+st.dataframe(df) 
+
+teamNumber = st.number_input('Team Number')
+st.write(teamNumber)
+
+sampleData1 = st.number_input(key = 1, label = 'Sample Data 1:')
+st.write(sampleData1)
+
+sampleData2 = st.number_input(key = 2, label = 'Sample Data 2:')
+st.write(sampleData2)
+
+sampleData3 = st.number_input(key = 3, label = 'Sample Data 3:')
+st.write(sampleData3)
