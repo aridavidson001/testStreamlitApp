7f7910f7f4aad250ce664867a226dd12d36488c3
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-10 12:18:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 
 st.header('Test for Importing and Exporting Data to and from Google Sheets')
 data = pd.read_csv("https://docs.google.com/spreadsheets/d/1RPBl2AuY5UVqp_ay8huKWGYNlIfRM2Zb6KbK2I28xrs/gviz/tq?tqx=out:csv")
