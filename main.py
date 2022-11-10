32b16789ad47773d5b9580b2624a5d06f42f0fee
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-10 15:32:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
+# streamlit_app.py
+
 import streamlit as st
-import pandas as pd
+from gsheetsdb import connect
 
-st.header('Test for Importing and Exporting Data to and from Google Sheets')
+# Create a connection object.
+conn = connect()
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
 sheet_url = st.secrets["public_gsheets_url"]
-data = pd.read_csv(sheet_url)
-df = pd.DataFrame(data)
-st.dataframe(df) 
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-teamNumber = st.number_input('Team Number')
-st.write(teamNumber)
-
-sampleData1 = st.number_input(key = 1, label = 'Sample Data 1:')
-st.write(sampleData1)
-
-sampleData2 = st.number_input(key = 2, label = 'Sample Data 2:')
-st.write(sampleData2)
-
-sampleData3 = st.number_input(key = 3, label = 'Sample Data 3:')
-st.write(sampleData3)
-
+# Print results.
+for row in rows:
+    st.write(f"{row.name} has a :{row.pet}:")
