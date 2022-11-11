c126959e621d45b048142f8a02e325fdfaa8782f
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-11 14:10:39 -0500

    Update About_Me.py

--- pages/About_Me.py
+++ pages/About_Me.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.markdown("# About Me")
 st.sidebar.markdown("# About Me")
-st.heading("Hello!")
+st.header("Hello!")
 st.write('''My name is Ari Davidson. I am a freshman at Chapel Hill High School and I love all things science.''')
