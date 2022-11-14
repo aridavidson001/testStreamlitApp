a1b6e2257ec8bd5420b6b113198e0158e808e148
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-14 09:39:19 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -12,11 +12,12 @@ st.write("This is where I will post completed projects, information about myself
 st.container()
 col1, col2 = st.columns(2)
 col1.header("About Me")
-col1.expander('Expander')
-with col1.expander('Expand'):
-  col1.write('Expanded ')
+with col1():
+  col1.expander('Expander')
+  with col1.expander('Expand'):
+    col1.write('Expanded ')
 
-#col2.header("Projects")
+col2.header("Projects")
  # col2.expander("Click to learn more")
   # with col2.expander('Expand'):
    #   st.write('Expanded ')
