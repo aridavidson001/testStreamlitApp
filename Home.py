d557acdf4eb4a8a9664bc1dafaa6137bfe806c13
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-14 09:49:23 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,18 +6,20 @@ import pandas as pd
 st.markdown("# Home")
 st.sidebar.markdown("# Home")
 
-st.header("Welcome to my amazing Website!")
-st.write("This is where I will post completed projects, information about myself, and more!")
+st.header('Welcome to my amazing Website!')
+st.write('This is where I will post completed projects, information about myself, and more!')
 
 st.container()
 col1, col2 = st.columns(2)
 with col1:
-  #st.header("About Me")
+  #st.header('About Me')
   st.expander('Expander')
   with st.expander('About Me'):
     st.subheader('My Name Is Ari Davidson')
+    st.write('I am currently teaching my self full-stack web development.')
 with col2:
-  #st.header("Projects")
+  #st.header('Projects')
   st.expander('Expander')
   with st.expander('Projects'):
     st.subheader('Currently Working On...')
+    st.write('Stuffs')
