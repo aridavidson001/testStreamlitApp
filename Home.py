39b7108c889a6b5f57f2d66dab4c3ece9446dec5
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-14 09:19:03 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -9,8 +9,6 @@ st.sidebar.markdown("# Home")
 st.header("Welcome to my amazing Website!")
 st.write("This is where I will post completed projects, information about myself, and more!")
 
-st.form('my_form_identifier')
-   st.form_submit_button('Submit to me')
 st.container()
 st.columns(spec)
 col1, col2 = st.columns(2)
