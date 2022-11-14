d05276f3a0e103139f2b76a337739779f67bbb9a
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-14 09:51:15 -0500

    Update About_Me.py

--- pages/About_Me.py
+++ pages/About_Me.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.markdown("# About Me")
 st.sidebar.markdown("# About Me")
 st.header("Hello!")
-st.write('''My name is Ari Davidson.''')
+st.write('''My name is Ari Davidson. I am currently teaching my self full-stack web development.''')
